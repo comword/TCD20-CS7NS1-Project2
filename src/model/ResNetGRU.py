6060dd79fe40290ef0962cd8702c26c1a01ac62a
comword <comword@live.com>
2020-11-13 17:27:54 +0000

    ResNetGRU

---
+++ src/model/ResNetGRU.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms.functional import to_tensor, to_pil_image
+from torchvision.models import resnet18
+
+import random
+import numpy as np
+from collections import OrderedDict
+
+
+class ResNetGRU(nn.Module):
+    def __init__(self, n_classes, input_shape, rnn_hidden_size=256, dropout=0.1):
+        
+        super(ResNetGRU, self).__init__()
+        self.n_classes = n_classes
+        self.input_shape = tuple(input_shape)
+        self.rnn_hidden_size = rnn_hidden_size
+        self.dropout = dropout
+        
+        # CNN Part 1
+        resnet = resnet18(pretrained=True)
+        resnet_modules = list(resnet.children())[:-3]
+        self.cnn_p1 = nn.Sequential(*resnet_modules)
+        
+        # CNN Part 2
+        self.cnn_p2 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.linear1 = nn.Linear(self.infer_features(), 256)
+        
+        # RNN
+        self.rnn1 = nn.GRU(input_size=rnn_hidden_size, 
+                            hidden_size=rnn_hidden_size,
+                            bidirectional=True, 
+                            batch_first=True)
+        self.rnn2 = nn.GRU(input_size=rnn_hidden_size, 
+                            hidden_size=rnn_hidden_size,
+                            bidirectional=True, 
+                            batch_first=True)
+        self.linear2 = nn.Linear(self.rnn_hidden_size*2, n_classes)
+    
+    def infer_features(self):
+        x = torch.zeros((1,) + self.input_shape)
+        x = self.cnn_p1(x)
+        x = self.cnn_p2(x)
+        x = x.permute(0, 3, 1, 2)
+        x_size = x.size(0)
+        T = x.size(1)
+        x = x.view(x_size, T, -1)
+
+        return x.shape[2]
+        
+    def forward(self, batch):
+        
+        batch = self.cnn_p1(batch)
+        # print(batch.size()) # torch.Size([-1, 256, 4, 13])
+        
+        batch = self.cnn_p2(batch) # [batch_size, channels, height, width]
+        # print(batch.size())# torch.Size([-1, 256, 4, 10])
+        
+        batch = batch.permute(0, 3, 1, 2) # [batch_size, width, channels, height]
+        # print(batch.size()) # torch.Size([-1, 10, 256, 4])
+         
+        batch_size = batch.size(0)
+        T = batch.size(1)
+        batch = batch.view(batch_size, T, -1) # [batch_size, T==width, num_features==channels*height]
+        # print(batch.size()) # torch.Size([-1, 10, 1024])
+        
+        batch = self.linear1(batch)
+        # print(batch.size()) # torch.Size([-1, 10, 256])
+        
+        batch, hidden = self.rnn1(batch)
+        feature_size = batch.size(2)
+        batch = batch[:, :, :feature_size//2] + batch[:, :, feature_size//2:]
+        # print(batch.size()) # torch.Size([-1, 10, 256])
+        
+        batch, hidden = self.rnn2(batch)
+        # print(batch.size()) # torch.Size([-1, 10, 512])
+        
+        batch = self.linear2(batch)
+        # print(batch.size()) # torch.Size([-1, 10, 20])
+        
+        batch = batch.permute(1, 0, 2) # [T==10, batch_size, num_classes==num_features]
+        # print(batch.size()) # torch.Size([10, -1, 20])
+        
+        return batch
+
+if __name__ == "__main__":
+    n_classes = 63
+    # height, width = 64, 128
+    height, width = 80, 160
+    model = ResNetGRU(n_classes, input_shape=(3, height, width))
+    print(model.infer_features())
+    inputs = torch.zeros((1, 3, height, width))
+    outputs = model(inputs)
+    print(outputs.shape)
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,3 +2,4 @@ from . import loss_entry, lr_entry, metric_entry
 
 from .CaptchaCNN import CaptchaCNN
 from .ResNetCRNN import ResNetCRNN
+from .ResNetGRU import ResNetGRU
